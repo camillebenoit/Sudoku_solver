08a9dad5a1bc69cab42aa9290ca81efa9dc81b37
Camille Benoit <camille.benoit@mines-ales.org>
2021-02-22 22:15:47 -0500

    Update __main__.py
    minor changes

--- __main__.py
+++ __main__.py
@@ -12,7 +12,12 @@ print(var1.get_neighbours_position())
 
 
 def main():
-    pass
+    number_sudoku = "1"
+    level = "beginers"
+    path_to_file = "sudoku_" + level + "\sudoku" + number_sudoku + ".ss"
+    sud = sk.Sudoku(path_to_file)
+    sud.backtracking_search()
+    print(sud)
 
 
 if __name__ == "__main__":
