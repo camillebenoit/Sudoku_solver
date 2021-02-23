896a6e2bd9841bd79d81fba51cde311c8ef76c5c
Camille Benoit <camille.benoit@mines-ales.org>
2021-02-22 22:16:02 -0500

    Update variable.py
    def de equal

--- variable.py
+++ variable.py
@@ -55,4 +55,6 @@ class Variable:
         neighbours_position_list.remove([nb_line, nb_col])
         return neighbours_position_list
 
-
+    def __eq__ (self,other):
+        #on compare les positions des variables
+        return self.position == other.position 
