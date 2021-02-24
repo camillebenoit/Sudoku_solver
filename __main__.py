ae40b31f0360fce2bbd3c5072a5e96be02b3e81a
Camille Benoit <camille.benoit@mines-ales.org>
2021-02-24 18:51:56 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,18 +1,18 @@
 import time
 import click
 
-from Sudoku import variable as vr
-from Sudoku import sudoku as sk
+import variable as vr
+import sudoku as sk
 
 var1 = vr.Variable(2, 2, 5)
 
 
-@clickcommand
+#@clickcommand
 
 def main():
     number_sudoku = "1"
     level = "beginners"
-    path_to_file = "Sudoku/sudoku_" + level + "/sudoku" + number_sudoku + ".ss"
+    path_to_file = "sudoku_" + level + "/sudoku" + number_sudoku + ".ss"
     sud = sk.Sudoku(path_to_file)
 
     print(sud)
